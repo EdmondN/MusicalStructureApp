721bed96c0a5d87dc74e320eaad30faec83c2071
EdmondN <eddimp3@hotmail.com>
2018-03-02 15:40:05 +0200

    Eddie

--- app/src/main/java/com/example/android/musicalstructureapp/PlayerActivity.java
+++ app/src/main/java/com/example/android/musicalstructureapp/PlayerActivity.java
@@ -3,6 +3,7 @@ package com.example.android.musicalstructureapp;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
 import android.widget.Toast;
 
 public class PlayerActivity extends AppCompatActivity {
@@ -10,10 +11,31 @@ public class PlayerActivity extends AppCompatActivity {
     String currentlyChartNumber;
     String currentlyTrackId;
 
+    //Go Back To The Music List.
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            // Respond to the action bar's Up/Home button
+            case android.R.id.home:
+                onBackPressed();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        Toast.makeText(this, "You Are Back to The Top 10 Chart List", Toast.LENGTH_SHORT).show();
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.player_activity);
+
+        getSupportActionBar().setHomeButtonEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         Intent intent = getIntent();
         Bundle receivedData = intent.getExtras();
